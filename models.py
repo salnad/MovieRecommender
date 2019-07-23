b50844791b7e0f4405611feef8bebd98e475759f
Nyia Noel George <cssi@cssis-MacBook-Pro.local>
2019-07-23 11:30:31 -0700

    Please work, pretty pretty please

--- models.py
+++ models.py
@@ -1,13 +1,11 @@
 from google.appengine.ext import ndb
 
-<<<<<<< HEAD
 class User(ndb.Model):
     first_name = ndb.IntegerProperty()
     last_name = ndb.IntegerProperty()
     email = ndb.StringProperty()
-=======
+    
 class Movie(ndb.Model):
     title = ndb.StringProperty(required=True)
     plot = ndb.StringProperty(required=True)
     director = ndb.StringProperty(required=True)
->>>>>>> e510714c7836c7b0c63a70837e8068bc5743c36b
