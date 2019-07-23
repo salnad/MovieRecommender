40e273848097d81b3069b4a51b8e38e9472cf55d
Tremael Marquis Arrington <timewizard2500@gmail.com>
2019-07-23 09:07:37 -0700

    Tremael changed main.py

--- main.py
+++ main.py
@@ -14,6 +14,24 @@ class MainPage(webapp2.RequestHandler):
     def get(self): #for a GET request
         self.response.write('Hello, World!') #the response
 
+class DataPage(webapp2.RequestHandler):
+    def post(self):
+        movie_title = self.request.get('Title: ')
+        movie_plot = self.request.get('Plot: ')
+        movie_director = self.request.get('Director: ')
+        }
+        #create movie object
+        movie_info = Movie(
+            title = movie_title,
+            plot = movie_plot,
+            director = movie_director
+        )
+        #store it in data store
+        movie_info.put()
+
+        result_template = jinja_env.get_template('results.html')
+        self.response.write(result_template.render(data_dict))
+
 #the app configuration section
 app = webapp2.WSGIApplication([
     ('/', MainPage), #this maps the root url to the MainPage Handler
